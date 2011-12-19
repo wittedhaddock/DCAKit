bf257f1294012775af2d5f2d2a5524d4dfb47ee9
Drew Crawford <drew@sealedabstract.com>
2011-12-19 15:23:32 -0600

    Initial commit

---
+++ CoreDataHelp.h
@@ -0,0 +1,47 @@
+//
+//  CoreDataHelp.h
+//  SRecovery
+//
+//  Created by Drew Crawford on 7/20/10.
+//  Copyright 2010 DrewCrawfordApps LLC. All rights reserved.
+//  May have been transferred or licensed to another party under the
+//  terms of a copyright release, if applicable.////
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+
+@interface CoreDataHelp : NSObject {
+	NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+}
+@property (readonly) NSManagedObjectContext *managedObjectContext;
+
++(NSManagedObjectContext*) moc;
++ (void) write;
++ (id) createObjectWithClass:(Class) c;
++ (void)enable_unit_test_mode;
+
+#if !(defined (DCA_RELEASE)) && !(defined(DCA_DEBUG)) && !defined(DCA_UNITTEST)
+#error You are need to define one or more of the following symbols to compile LogBuddy:  RELEASE, DEBUG, UNITTEST
+#endif
+
+
+#if (defined(DCA_RELEASE) && defined(DCA_DEBUG)) || (defined(DCA_RELEASE) && defined(DCA_UNITTEST)) || (defined(DCA_DEBUG) && defined(DCA_UNITTEST))
+#error Too many symbols!
+#endif
+
+//use this macro to start CoreDataHelp
+
+#ifdef DCA_DEBUG
+#define COREDATAHELP_START
+#endif
+#ifdef DCA_RELEASE
+#define COREDATAHELP_START
+#endif
+#ifdef DCA_UNITTEST
+#define COREDATAHELP_START [CoreDataHelp enable_unit_test_mode]
+#endif
+@end
