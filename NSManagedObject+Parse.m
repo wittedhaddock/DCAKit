5f0d707ed836fbf11eb3ab23eb6ffd7e5e73cb92
Drew Crawford <drew@sealedabstract.com>
2011-12-29 18:21:43 -0600

    it basically works

--- NSManagedObject+Parse.m
+++ NSManagedObject+Parse.m
@@ -10,10 +10,13 @@
 #import <Parse/Parse.h>
 #import "CoreDataHelp.h"
 @implementation NSManagedObject (Parse)
+- (NSString*) coreDataID {
+    return [[[self objectID] URIRepresentation] description];
+
+}
 - (PFObject*) _getServerPFObject {
     PFQuery *query = [PFQuery queryWithClassName:NSStringFromClass([self class])];
-    NSString *coreDataID = [[[self objectID] URIRepresentation] description];
-    [query whereKey:@"coreDataID" equalTo:coreDataID];
+    [query whereKey:@"coreDataID" equalTo:self.coreDataID];
     NSArray *resultArray = [query findObjects];
     NSAssert(resultArray.count<=1, @"Too many results: %@", resultArray);
     if (resultArray.count==0) return nil;
@@ -23,10 +26,32 @@
     NSDictionary *myDict =  [[self entity] attributesByName];
     for (id key in [myDict allKeys]) {
         id val = [self valueForKey:key];
+        if (!val) val = [NSNull null]; //pfobject doens't like null
+        if ([val isKindOfClass:[UIImage class]]) {
+            NSData *imageData = UIImageJPEGRepresentation(val, 0.8);
+            PFFile *file = [PFFile fileWithName:@"key.jpg" data:imageData];
+            [file save];
+            val = file;
+        }
+        NSLog(@"%@=%@",key,val);
+
         [p setObject:val forKey:key];
     }
-    NSString *coreDataID = [[[self objectID] URIRepresentation] description];
-    [p setObject:coreDataID forKey:@"coreDataID"];
+    myDict = [[self entity] relationshipsByName];
+    for (id key in [myDict allKeys]) {
+        NSMutableString *valRef = [NSMutableString string];
+        NSManagedObject *remote = [self valueForKey:key];
+        if ([remote isKindOfClass:[NSSet class]]) {
+            NSSet *remote_s = (NSSet*) remote;
+            for (NSManagedObject *obj in remote_s) {
+                [valRef appendFormat:@"%@,",obj.coreDataID];
+            }
+        }
+        else [valRef appendFormat:@"%@",remote.coreDataID];
+        NSLog(@"%@=%@",key,valRef);
+        [p setObject:valRef forKey:key];
+    }
+    [p setObject:self.coreDataID forKey:@"coreDataID"];
 
 }
 - (PFObject*) _createNewObject {
@@ -36,6 +61,8 @@
 }
 
 - (void) sync {
+    [CoreDataHelp write];
+    NSLog(@"Syncing %@",self);
     PFObject *serverObj = [self _getServerPFObject];
     if (!serverObj) {
         serverObj = [self _createNewObject];
