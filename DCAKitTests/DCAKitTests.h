617c8ae6a7219e2223ede3ea3891602b5409c79c
Bion Oren <bion@drewcrawfordapps.com>
2012-01-03 13:25:52 -0600

    Initial commit

---
+++ DCAKitTests/DCAKitTests.h
@@ -0,0 +1,13 @@
+//
+//  DCAKitTests.h
+//  DCAKitTests
+//
+//  Created by Bion Oren on 1/3/12.
+//  Copyright (c) 2012 DrewCrawfordApps. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DCAKitTests : SenTestCase
+
+@end
---
+++ DCAKitTests/DCAKitTests.m
@@ -0,0 +1,32 @@
+//
+//  DCAKitTests.m
+//  DCAKitTests
+//
+//  Created by Bion Oren on 1/3/12.
+//  Copyright (c) 2012 DrewCrawfordApps. All rights reserved.
+//
+
+#import "DCAKitTests.h"
+
+@implementation DCAKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DCAKitTests");
+}
+
+@end
